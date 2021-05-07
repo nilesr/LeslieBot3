4b93ebde1a94aa8f9fbf1fb36fcf926120ba1110
Niles Rogoff <me@niles.xyz>
2021-05-07 14:55:52 -0700

    Add a bunch of selectors, some trivia

--- random.cpp
+++ random.cpp
@@ -9,30 +9,241 @@ int serial(const char* fmt, ...);
 
 int random();
 
-char* random_smasher() {
-	const smasher_t* smasher = smashers + random() % N_SMASHERS;
-	loadLogo(smasher->franchize);
-	char* result = nrprintf("Play %s", smasher->name);
-	return result;
-}
-
 char* random_challenge() {
-	int sum_weights = 0;
-	for (int i = 0; i < N_SELECTOR; i++) {
-		sum_weights += SELECTORS[i].weight;
-	}
+  int sum_weights = 0;
+  for (int i = 0; i < N_SELECTOR; i++) {
+    sum_weights += SELECTORS[i].weight;
+  }
   int rand = random() % sum_weights;
-	int i = 0;
-	while (rand >= SELECTORS[i].weight) {
-		rand -= SELECTORS[i].weight;
-		i += 1;
-	}
-	serial("Using randomizer at index %d\r\n", i);
-	return (*SELECTORS[i].randomizer)();
+  int i = 0;
+  while (rand >= SELECTORS[i].weight) {
+    rand -= SELECTORS[i].weight;
+    i += 1;
+  }
+  serial("Using randomizer at index %d\r\n", i);
+  // Just in case the randomizer we select doesn't set a logo
+  loadLogo(SMASH);
+  return (*SELECTORS[i].randomizer)();
 }
 
 char* random_trivia() {
-	loadLogo(SMASH);
-	// TODO this wil not be the final implementation
-	return strdup(String{trivia(random() % N_TRIVIA)}.c_str());
+  loadLogo(SMASH);
+  // TODO this wil not be the final implementation
+  return strdup(String{trivia(random() % N_TRIVIA)}.c_str());
+}
+
+char* random_smasher() {
+  const smasher_t* smasher = smashers + random() % N_SMASHERS;
+  loadLogo(smasher->franchize);
+  char* result = nrprintf("Play %s", smasher->name);
+  return result;
+}
+
+char* random_letter() {
+  char letter;
+  int total;
+  const smasher_t* randomly_selected_smasher;
+  // There are no smashers with a Q in their name
+  do {
+    letter = 'A' + (random() % 26);
+    total = 0;
+    for (int i = 0; i < N_SMASHERS; i++) {
+      if (strchr(smashers[i].name, letter) || strchr(smashers[i].name, tolower(letter))) {
+	total++;
+	if (random() % total == 0) {
+	  randomly_selected_smasher = smashers + i;
+	}
+      }
+    }
+  } while (total == 0);
+  loadLogo(randomly_selected_smasher->franchize);
+  return nrprintf("Play someone with a %c in their name (%d total)", letter, total);
+}
+
+char* random_smasher_diagonal() {
+  const smasher_t* smasher = smashers + random() % N_SMASHERS;
+  loadLogo(smasher->franchize);
+  return nrprintf("Play someone diagonal from %s", smasher->name);
+}
+
+char* random_smasher_horizontal() {
+  const smasher_t* smasher = smashers + random() % N_SMASHERS;
+  loadLogo(smasher->franchize);
+  return nrprintf("Play someone left or right from %s", smasher->name);
+}
+
+char* random_smasher_vertical() {
+  const smasher_t* smasher = smashers + random() % N_SMASHERS;
+  loadLogo(smasher->franchize);
+  return nrprintf("Play someone up or down from %s", smasher->name);
+}
+
+char* random_franchize() {
+  franchize_t franchize = (franchize_t) (random() % N_FRANCHIZE);
+  int total = 0;
+  for (int i = 0; i < N_SMASHERS; i++) {
+    if (smashers[i].franchize == franchize) {
+      total++;
+    }
+  }
+  loadLogo(franchize);
+  return nrprintf("Play someone from this franchize (%d total)", total);
+}
+
+char* last_three_matches() {
+  return strdup("Play someone you played against in the last 3 matches");
+}
+
+char* prom() {
+  return strdup("Play someone you would be willing to go to prom with");
+}
+
+char* cant_look_sans() {
+  return strdup("Play someone who can't look sans in the eyes");
+}
+
+char* random_smasher_horizontal_exact() {
+  const int offset = (random() % 7) + 1;
+  const char* direction = (random() % 2) == 0 ? "left" : "right";
+  return nrprintf("Play the character %d to the %s from whoever you last played as", direction, offset);
+}
+
+char* therapy() {
+  return strdup("Play someone who needs therapy");
+}
+
+char* sus() {
+  return strdup("Play the most \"sus\" character");
+}
+
+char* light_green() {
+  return strdup("Play the character who would pick light green in among us");
+}
+
+char* infallable() {
+  return strdup("Play someone who has never done anything wrong, bless their hearts");
+}
+
+char* recent_seen_fanart() {
+  return strdup("Play the character that you last saw fanart of");
+}
+
+char* recent_drawn_fanart() {
+  return strdup("Play the character that you last drew fanart of");
+}
+
+char* zelda_arm_wrestling() {
+  return strdup("Play someone who zelda could beat in an arm wrestling match");
+}
+
+char* midpoint_enemies() {
+  return strdup("Play someone in the middle of the characters who would not like each other");
+}
+
+char* leslie() {
+  return strdup("Play the character that Leslie would make you play");
+}
+
+char* want_added_to_botw2() {
+  return strdup("Play the character you would most like added to BOTW 2");
+}
+
+char* most_chaotic() {
+  return strdup("Play the most chaotic character");
+}
+
+char* least_chaotic() {
+  return strdup("Play the least chaotic character");
+}
+
+char* inflated_ego() {
+  return strdup("Play someone who thinks they are cooler than they really are");
+}
+
+char* not_played_today() {
+  return strdup("Play someone you haven't played yet today");
+}
+
+char* surprised_if_transported_to_reality() {
+  return strdup("Play the fighter who would be most suprised if they were transported to reality");
+}
+
+char* never_played_franchize() {
+  return strdup("Play someone from a video game you've never played and never want to play");
+}
+
+char* same_franchize_as_last_game_played() {
+  return strdup("Play a character from the same franchize as the last game you've played");
+}
+
+char* would_cry_at_last_character() {
+  return strdup("Play someone who would cry if they saw your last character");
+}
+
+char* likes_owls() {
+  return strdup("Play the character who most likes owls");
+}
+
+char* anime() {
+  return strdup("Play an anime character");
+}
+
+char* buff() {
+  return strdup("Play a buff character");
+}
+
+char* probably_banned_from_tournaments() {
+  return strdup("Play someone you think should be banned from smash competitions");
+}
+
+char* best_duck() {
+  return strdup("Play the character that would survive the best if they were turned into a duck");
+}
+
+char* worst_duck() {
+  return strdup("Play the character that would survive the worst if they were turned into a duck");
+}
+
+char* cant_tie_shoes() {
+  return strdup("Play a character that probably doesn't know how to tie their shoes");
+}
+
+char* best_professor() {
+  return strdup("Play a character who you would want as a professor, and isn't a teacher");
+}
+
+char* afraid_of() {
+  return strdup("Play a character who you are afraid of");
+}
+
+char* would_beat_up_last_character() {
+  return strdup("Play a character that would beat up the character you last played as");
+}
+
+char* confused_by_rickroll() {
+  return strdup("Play a character that would be confused by a rick roll");
+}
+
+char* least_vowels() {
+  return strdup("Play the character with the least number of vowels in their name");
+}
+
+char* most_vowels() {
+  return strdup("Play the character with the most number of vowels in their name");
+}
+
+char* dab() {
+  return strdup("Play a character who would unironically dab");
+}
+
+char* negative_int() {
+  return strdup("Play a character who has a negative intelligence stat");
+}
+
+char* negative_chr() {
+  return strdup("Play a character who has a negative charisma stat");
+}
+
+char* hottest_take() {
+  return strdup("Play the character you have the hottest of takes on");
 }
--- random.h
+++ random.h
@@ -6,17 +6,105 @@
 typedef char* (randomizer_t)();
 
 randomizer_t random_smasher;
+randomizer_t random_smasher;
+randomizer_t random_letter;
+randomizer_t random_smasher_diagonal;
+randomizer_t random_smasher_horizontal;
+randomizer_t random_smasher_vertical;
+randomizer_t random_franchize;
+randomizer_t last_three_matches;
+randomizer_t prom;
+randomizer_t cant_look_sans;
+randomizer_t random_smasher_horizontal_exact;
+randomizer_t therapy;
+randomizer_t sus;
+randomizer_t light_green;
+randomizer_t infallable;
+randomizer_t recent_seen_fanart;
+randomizer_t recent_drawn_fanart;
+randomizer_t zelda_arm_wrestling;
+randomizer_t midpoint_enemies;
+randomizer_t leslie;
+randomizer_t want_added_to_botw2;
+randomizer_t most_chaotic;
+randomizer_t least_chaotic;
+randomizer_t inflated_ego;
+randomizer_t not_played_today;
+randomizer_t surprised_if_transported_to_reality;
+randomizer_t never_played_franchize;
+randomizer_t same_franchize_as_last_game_played;
+randomizer_t would_cry_at_last_character;
+randomizer_t likes_owls;
+randomizer_t anime;
+randomizer_t buff;
+randomizer_t probably_banned_from_tournaments;
+randomizer_t best_duck;
+randomizer_t worst_duck;
+randomizer_t cant_tie_shoes;
+randomizer_t best_professor;
+randomizer_t afraid_of;
+randomizer_t would_beat_up_last_character;
+randomizer_t confused_by_rickroll;
+randomizer_t least_vowels;
+randomizer_t most_vowels;
+randomizer_t dab;
+randomizer_t negative_int;
+randomizer_t negative_chr;
+randomizer_t hottest_take;
 
 typedef struct selector_t {
 	int weight;
 	randomizer_t* randomizer;
 } selector_t;
 
-#define N_SELECTOR 1
+#define N_SELECTOR 46
 #ifdef DEFINE_SELECTORS
 selector_t SELECTORS[N_SELECTOR] {
-	{1, &random_smasher}
-	// TODO more
+	{15, &random_smasher},
+	{20, &random_letter},
+	{20, &random_smasher_diagonal},
+	{20, &random_smasher_horizontal},
+	{20, &random_smasher_vertical},
+	{20, &random_franchize},
+	{10, &last_three_matches},
+	{1, &prom},
+	{1, &cant_look_sans},
+	{10, &random_smasher_horizontal_exact},
+	{2, &therapy},
+	{1, &sus},
+	{1, &light_green},
+	{2, &infallable},
+	{5, &recent_seen_fanart},
+	{1, &recent_drawn_fanart},
+	{2, &zelda_arm_wrestling},
+	{5, &midpoint_enemies},
+	{10, &leslie},
+	{1, &want_added_to_botw2},
+	{1, &most_chaotic},
+	{1, &least_chaotic},
+	{5, &inflated_ego},
+	{10, &not_played_today},
+	{1, &surprised_if_transported_to_reality},
+	{5, &never_played_franchize},
+	{5, &same_franchize_as_last_game_played},
+	{5, &would_cry_at_last_character},
+	{1, &likes_owls},
+	{5, &anime},
+	{5, &buff},
+	{10, &probably_banned_from_tournaments},
+	{1, &best_duck},
+	{1, &worst_duck},
+	{2, &cant_tie_shoes},
+	{2, &best_professor},
+	{2, &afraid_of},
+	{5, &would_beat_up_last_character},
+	{2, &confused_by_rickroll},
+	{1, &least_vowels},
+	{1, &most_vowels},
+	{5, &dab},
+	{5, &negative_int},
+	{5, &negative_chr},
+	{1, &hottest_take},
 };
 #else
 extern selector_t SELECTORS[N_SELECTOR];
